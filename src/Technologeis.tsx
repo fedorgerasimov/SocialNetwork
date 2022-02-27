6276752f7416e18c3b6a889082bcb78538b774bd
fedorgerasimov <1031577@mail.ru>
2022-02-27 13:22:31 +0300

    create component

---
+++ src/Technologeis.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Technologeis = () => {
+    return (<div>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+
+    )
+}
+
