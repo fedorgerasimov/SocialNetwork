bf51853a1782cb2f2766959086950712632e7ef9
fedorgerasimov <1031577@mail.ru>
2022-03-19 17:08:13 +0300

    In component Dialogs added CSS to message and dialog

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -7,8 +7,18 @@ type DialogsType  ={
 
 export const Dialogs = (props: DialogsType) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={`${s.dialog} ${s.active}`}>Jameson</div>
+                <div className={s.dialog}>Bob</div>
+                <div className={s.dialog}>Jane</div>
+                <div className={s.dialog}>Arnold</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>What's up?</div>
+                <div className={s.message}>What is your hobby?</div>
+                <div className={s.message}>Hello</div>
+            </div>
         </div>
     )
 }
