bed1c19c15ae6ffa1861b74af3d591a7760c8dae
fedorgerasimov <1031577@mail.ru>
2022-03-20 13:25:41 +0300

    In MyPosts.tsx added html tags

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -5,13 +5,18 @@ import Post from "./Posts/Post";
 
 const MyPosts = () => {
     return (
-        <div>
-            My posts
-        <div>
-            <textarea></textarea>
-            <button>Add post</button>
-            <button>Remove</button>
-        </div>
+        <div className={s.postsBlock}>
+            <h2>My posts</h2>
+            <div>
+                <div>
+                    <textarea></textarea>
+                </div>
+                <div>
+                    <button>Add post</button>
+                    <button>Remove</button>
+                </div>
+
+            </div>
             <div className={s.posts}>
                 <Post message={"Hi, what is favourite movie?"} likesCount={20}/>
                 <Post message={"It's the best social network you have ever seen"} likesCount={15}/>
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,20 +1,14 @@
 import React from "react";
 import s from './Profile.module.css'
 import MyPosts from "./MyPosts/MyPosts";
+import {ProfileInfo} from "./ProfileInfo/ProfileInfo";
 
-type ProfileType = {
-
-}
+type ProfileType = {}
 
 function Profile(props: any) {
     return (
         <div>
-            <div>
-                <img src="https://anjalisk96.files.wordpress.com/2017/09/top-social-networking-sites.jpg?w=693&zoom=2" alt="social-network"/>
-            </div>
-            <div>
-                ava + description
-            </div>
+            <ProfileInfo/>
             <MyPosts/>
         </div>
     )
