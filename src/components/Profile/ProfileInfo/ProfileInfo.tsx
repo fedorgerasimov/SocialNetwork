ea7d94b0366b511ecdfb10f67b50fd75fb6cfabd
fedorgerasimov <1031577@mail.ru>
2022-03-20 13:26:52 +0300

    Added new component ProfileInfo.tsx

---
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import s from './ProfileInfo.module.css'
+
+
+type ProfileInfo = {
+
+}
+
+export const  ProfileInfo =(props: any) =>{
+    return (
+        <div>
+            <div>
+                <img src="https://anjalisk96.files.wordpress.com/2017/09/top-social-networking-sites.jpg?w=693&zoom=2" alt="social-network"/>
+            </div>
+            <div className={s.descriptionBlock}>
+                ava + description
+            </div>
+
+        </div>
+    )
+}
+
