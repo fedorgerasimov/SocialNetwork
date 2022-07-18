f76aab7c8c457361bf41ca2251d9632caa75d34f
fedorgerasimov <1031577@mail.ru>
2022-07-18 12:32:55 +0300

    47. Making a copy of state in profile-reducer.ts

--- src/index.tsx
+++ src/index.tsx
@@ -6,18 +6,18 @@ import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
 
-export const rerenderEntireTree = () => {
-    ReactDOM.render(
-        <BrowserRouter>
-            <Provider store={store}>
-                <App/>
-            </Provider>
-        </BrowserRouter>,
-        document.getElementById('root')
-    )
-}
-rerenderEntireTree() // нужно вызвать функцию, чтобы произошла перерисовка
-store.subscribe(() => {
-    let state = store.getState()
+ReactDOM.render(
+    <BrowserRouter>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </BrowserRouter>,
+    document.getElementById('root')
+)
+
+//rerenderEntireTree() // нужно вызвать функцию, чтобы произошла перерисовка
+
+/*store.subscribe(() => {  убрали подписку, чтобы каждый раз не перерисовывать дерево при любом изменение
+   // let state = store.getState()
     rerenderEntireTree()
-})
+})*/
--- src/redux/profile-reducer.ts
+++ src/redux/profile-reducer.ts
@@ -1,52 +1,58 @@
 import {ActionsTypes, PostsType, ProfilePageType} from "./store";
 
-export type ActionsProfileTypes =
-    ReturnType<typeof addPostAC>
-    | ReturnType<typeof updateNewPostTextAC>
-
-export const addPostAC = () => ({type: 'ADD-POST',} as const)
-export const updateNewPostTextAC = (newText: string) => ({type: 'UPDATE-NEW-POST-TEXT', newText} as const)
 
 let initialState = {
-            messageForNewPost: '',
-            posts: [
-                {
-                    id: 1,
-                    message: 'Hi, what is favourite movie?',
-                    likesCount: 20,
-                    avatar: "https://cdn4.iconfinder.com/data/icons/smileys-for-fun/128/smiley__9-512.png"
-                },
-                {
-                    id: 2,
-                    message: 'It\'s the best social network you have ever seen',
-                    likesCount: 15,
-                    avatar: "https://cdn4.iconfinder.com/data/icons/smileys-for-fun/128/smiley__12-512.png"
-                },
-                {
-                    id: 3,
-                    message: 'Weather forecast for friday',
-                    likesCount: 10,
-                    avatar: "https://cdn4.iconfinder.com/data/icons/smileys-for-fun/128/smiley__21-512.png"
-                }
-            ],
+    messageForNewPost: '',
+    posts: [
+        {
+            id: 1,
+            message: 'Hi, what is favourite movie?',
+            likesCount: 20,
+            avatar: "https://cdn4.iconfinder.com/data/icons/smileys-for-fun/128/smiley__9-512.png"
+        },
+        {
+            id: 2,
+            message: 'It\'s the best social network you have ever seen',
+            likesCount: 15,
+            avatar: "https://cdn4.iconfinder.com/data/icons/smileys-for-fun/128/smiley__12-512.png"
+        },
+        {
+            id: 3,
+            message: 'Weather forecast for friday',
+            likesCount: 10,
+            avatar: "https://cdn4.iconfinder.com/data/icons/smileys-for-fun/128/smiley__21-512.png"
         }
+    ],
+}
 
-export const profileReducer = (state: ProfilePageType= initialState, action: ActionsTypes):ProfilePageType => {
+export const profileReducer = (state: ProfilePageType = initialState, action: ActionsTypes): ProfilePageType => {
     switch (action.type) {
-        case 'ADD-POST':
+        case 'ADD-POST': {
             const newPost: PostsType = {
                 id: new Date().getTime(),
                 message: state.messageForNewPost,
                 likesCount: 5,
                 avatar: 'https://cdn4.iconfinder.com/data/icons/emojis-flat-pixel-perfect/64/emoji-64-512.png'
             }
-            state.posts.push(newPost)
-            return state
-        case 'UPDATE-NEW-POST-TEXT':
-            state.messageForNewPost = action.newText
-            return state
+            let stateCopy = {...state}
+            stateCopy.posts = [...state.posts]
+            stateCopy.posts.push(newPost)
+            stateCopy.messageForNewPost = ''
+            return stateCopy
+        }
+        case 'UPDATE-NEW-POST-TEXT': {
+            let stateCopy = {...state}
+            stateCopy.messageForNewPost = action.newText
+            return stateCopy
+        }
         default:
             return state;
-
     }
 }
+
+export type ActionsProfileTypes =
+    ReturnType<typeof addPostAC>
+    | ReturnType<typeof updateNewPostTextAC>
+
+export const addPostAC = () => ({type: 'ADD-POST',} as const)
+export const updateNewPostTextAC = (newText: string) => ({type: 'UPDATE-NEW-POST-TEXT', newText} as const)
