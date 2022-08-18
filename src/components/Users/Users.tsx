d7e300472e7ced0c2c0b2271ba13ab430d4c499e
fedorgerasimov <1031577@mail.ru>
2022-08-18 11:59:31 +0300

    52 Side Effect

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -5,27 +5,34 @@ import axios from "axios";
 
 
 export const Users = (props: UsersPropsType) => {
-
-    if (props.usersPage.users.length === 0) {
-            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                debugger
-                props.setUsers (response.data.items)
-            })
+    let getUsers = () => {
+        if (props.usersPage.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(response => {
+                    props.setUsers(response.data.items)
+                })
+        }
     }
 
     return (
         <div>
+             <button onClick={getUsers}>Get Users</button>
             {
                 props.usersPage.users.map(el => <div key={el.id}>
                     <span>
                         <div>
 
-                            <img src={el.photos.small != null ? el.photos.small : require('../assets/img/ava.jpg') } alt="" className={styles.userPhoto}/>
+                            <img src={el.photos.small != null ? el.photos.small : require('../assets/img/ava.jpg')}
+                                 alt="" className={styles.userPhoto}/>
                         </div>
                         <div>
                             {el.followed
-                                ? <button onClick={() => {props.unFollow(el.id)}}>Unfollow</button>
-                                : <button onClick={() => {props.follow(el.id)}}>Follow</button>
+                                ? <button onClick={() => {
+                                    props.unFollow(el.id)
+                                }}>Unfollow</button>
+                                : <button onClick={() => {
+                                    props.follow(el.id)
+                                }}>Follow</button>
                             }
                         </div>
                     </span>
